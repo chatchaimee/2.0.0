f117d5b750c6f9432c004f79d7a92d673fed4ce5
Chatchai Meesuksabai <chatchai.m@dv.co.th>
2021-06-24 23:17:45 +0700

    Change color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,13 +7,13 @@ const GlobalStyle = createGlobalStyle`
 
     /** ===== Colors ===== */
     // Change favorite color
-    --hue-color: 231; // Purple 250 - Green 142 - Blue 231 - Pink 340
+    --hue-color: 204; // Purple 250 - Green 142 - Blue 231 - Pink 340
 
     // HSL color mode
-    --first-color: hsl(var(--hue-color), 69%, 61%);
-    --first-color-second: hsl(var(--hue-color), 69%, 61%);
-    --first-color-alt: hsl(var(--hue-color), 57%, 53%);
-    --first-color-lighter: hsl(var(--hue-color), 92%, 85%);
+    --first-color: hsl(var(--hue-color), 62%, 28%);
+    --first-color-second: hsl(var(--hue-color), 62%, 34%);
+    --first-color-alt: hsl(var(--hue-color), 62%, 40%);
+    --first-color-lighter: hsl(var(--hue-color), 70%, 72%);
     --title-color: hsl(var(--hue-color), 8%, 15%);
     --text-color: hsl(var(--hue-color), 8%, 45%);
     --text-color-light: hsl(var(--hue-color), 8%, 65%);
