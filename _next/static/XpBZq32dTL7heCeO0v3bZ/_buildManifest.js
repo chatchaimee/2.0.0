0fc0f8a749e4a99536f5ee2f3daf447674ab57dd
Chatchai Meesuksabai <chatchai.m@dv.co.th>
2021-06-25 10:23:20 +0700

    Deploy 419028b2cbda885f54245127d6f00fc71a74b628 to GitHub pages [ci skip]

--- _next/static/XpBZq32dTL7heCeO0v3bZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4eba4dd33cd56403fe3e.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
